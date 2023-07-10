19c5599eb8433af048c612c0fc4f80ef97b9e3c2
Yunyang Lu <whitefeather4096@gmail.com>
2023-07-10 23:23:58 +0800

    main file updated

--- User/main.c
+++ User/main.c
@@ -1,22 +1,37 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
+#include "Encoder.h"
+#include "PWM.h"
+#include "Ctrl6612.h"
+
+int16_t Num;
+uint8_t Speed;
 
 int main(void)
 {
 	OLED_Init();
+	Encoder_Init();
+	CTRL_Init();
 	
-	OLED_ShowChar(1, 1, 'A');
-	OLED_ShowString(1, 3, "HelloWorld!");
-	OLED_ShowNum(2, 1, 12345, 5);
-	OLED_ShowSignedNum(2, 7, 12345, 5);
-	OLED_ShowHexNum(3, 1, 0xAA55, 4);
-	OLED_ShowBinNum(4, 1, 0xAA55, 16);
-	
-	OLED_Clear();
+	OLED_ShowString(1, 1, "Speed:");
+	OLED_ShowString(2, 1, "RPM:");
+
 	
 	while (1)
 	{
-
+		Num += Encoder_Get();
+		//OLED_ShowSignedNum(2, 7, Num, 3);
+		if (Num > 100)
+		{
+			Speed = 100;
+		} else if (Num < 0)
+		{
+			Speed = 0;
+		} else{
+			Speed = Num;
+		}
+		Motor_SetSpeed(Speed);
+		OLED_ShowSignedNum(1, 7, Speed, 3);
 	}
 }
